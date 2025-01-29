c356ead4323e57bff30948614f1a7033a31983e4
Latisha <latisha.mcneel@gmail.com>
2025-01-28 21:37:42 -0600

    Volunteer model

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
-const forceDatabaseRefresh = process.env.FORCE_DATABASE_REFRESH || false;
-
+import dotenv from 'dotenv';
 import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.json());
 app.use(routes);
 
+const forceDatabaseRefresh = process.env.FORCE_DATABASE_REFRESH === '1' || false;
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
