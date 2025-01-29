78dcc1ab260a29b0a422b705afcd0a32c1699e5c
Latisha <latisha.mcneel@gmail.com>
2025-01-28 20:59:16 -0600

    Volunteer routes are initialized

--- server/src/routes/api/volunteers/index.js
+++ server/src/routes/api/volunteers/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import volunteers from './volunteers/index.js';
 
-router.use('/volunteers', volunteers);
 router.get('/', (req, res) => {
-  res.send('Hello API!');
+  res.send('Hello Volunteers!');
 });
 
 export default router;
